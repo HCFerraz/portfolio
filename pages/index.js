06113ca61ec67b04ced0dd8b7ace0426f29ab4f9
HCFerraz <henriquecabralferraz@gmail.com>
2020-11-22 19:51:54 -0300

    Beggining

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-
-function Home() {
-    const [counter, setCounter] = useState(0)
+function HomePage() {
     return (
-        <>
-            <div style={{ display: "flex", flexDirection: "column", backgroundColor: "blueviolet", width: "100%", height: "100%", alignItems: "center", justifyContent: "center" }}>
-                <button style={{ width: "50%" }} onClick={() => setCounter(counter + 1)}>Click here</button>
-                <span>{counter}</span>
-            </div>
-        </>
+        <div className="container">
+            <p>This is a test</p>
+        </div>
     )
 }
-export default Home
