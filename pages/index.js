6c19d8c878b9fa3ce8d2f6fe6e7106bac9db6285
HCFerraz <henriquecabralferraz@gmail.com>
2020-11-22 19:46:01 -0300

    change

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
 import { useState } from 'react'
 
 function Home() {
-    const [counter, setCounter] = useState(0)
     return (
         <>
-            <div style={{ display: "flex", flexDirection: "column", backgroundColor: "blueviolet", width: "100%", height: "100%"}}>
-                <button onClick={() => setCounter(counter + 1)}>Click here</button>
-                <span>{counter}</span>
-            </div>
         </>
     )
 }
