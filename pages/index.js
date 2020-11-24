005e5cae8c28a4dc8f4f0cff664fb8e9d62328ae
HCFerraz <henriquecabralferraz@gmail.com>
2020-11-23 23:03:29 -0300

    Testando novas adicoes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import styles from '../components/layout.module.css'
+
 function Home() {
     return (
         <div className="container">
-            <p>This is wip</p>
+            <Head>
+                <title>Index.js</title>
+            </Head>
+            <p className={styles.back}>My portfolio</p>
+            Learn <Link href="/files">here</Link>
         </div>
     )
 }
