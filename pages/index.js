74cc6b8f52aab18050c2cc6b9488cd3691d021ed
HCFerraz <henriquecabralferraz@gmail.com>
2020-12-09 01:14:10 -0300

    Adding contact area

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import home from '../styles/home.module.css'
 import about from '../styles/about.module.css'
 import tech from '../styles/tech.module.css'
 import portfolio from '../styles/portfolio.module.css'
-
-import { Image } from '../components/UI/Image'
+import contact from '../styles/contact.module.css'
 
 import { useState, useEffect } from 'react'
 import { Player, ControlBar } from 'video-react'
@@ -18,6 +17,7 @@ export default function Home() {
                 <title>Henrique Cabral - Portfólio</title>
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed&display=swap" rel="homeheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet" />
                 <meta property="og:image" content="/images/about_me.svg"></meta>
                 <link rel="stylesheet" href="/css/video-react.css" />
             </Head>
@@ -26,28 +26,28 @@ export default function Home() {
                     <img src="/images/code.svg" alt="Coding..." width="120px" />
                     <nav>
                         <ul className={home.navigation_list_container}>
-                            <li><a href="#">Início</a></li>
-                            <li><a href="#">Sobre mim</a></li>
-                            <li><a href="#">Portfolio</a></li>
-                            <li><a href="#">Contato</a></li>
+                            <li><a href="#home">Início</a></li>
+                            <li><a href="#about">Sobre mim</a></li>
+                            <li><a href="#portfolio">Portfolio</a></li>
+                            <li><a href="#contact">Contato</a></li>
                         </ul>
                     </nav>
                 </div>
-                <div className={home.presentation_container}>
+                <div className={home.presentation_container} id="home">
                     <div className={home.information_container}>
                         <h1>Henrique Cabral Ferraz</h1>
                         <h2>Front-End Developer & Back End Enthusiastic</h2>
                         <p>Desenvolvimento de páginas estáticas, SPA, criação e consumo de APIs, e muito mais... </p>
                         <div className={home.buttons_container}>
-                            <a href="#">Portfolio</a>
+                            <a href="#portfolio">Portfolio</a>
                             <a href="https://github.com/HCFerraz" target="_blank">Github</a>
                         </div>
                     </div>
                     {size.width && size.width > '1024' ? <img src="/images/web_developer.svg" alt="Web Developer" /> : <img src="/images/web_devices.svg" alt="Web Devices" />}
                 </div>
             </div>
-            <div className={about.content_container}>
-                {size.width && size.width > '1160' ? <img src="/images/profile.svg" alt="About me" /> : null}
+            <div className={about.content_container} id="about">
+                {size.width && size.width > '1185' ? <img src="/images/profile.svg" alt="About me" /> : null}
                 <div className={about.information}>
                     <h1>Sobre</h1>
                     <p>Olá, me chamo Henrique e atualmente curso Análise e Desenvolvimento de Sistemas no IFSP. Adoro saber como as coisas funcionam e amo criar coisas novas, a partir disso, descobri na programação o meu espaço.</p>
@@ -58,12 +58,11 @@ export default function Home() {
             <div className={tech.content_container}>
                 <h1>Focando minhas energias em aprender mais sobre os seguintes frameworks:</h1>
                 <div>
-                    <img src="/images/next_js.svg" />
+                    <img src="/images/nextblue.svg" />
                     <img src="/images/react_js.svg" />
                 </div>
             </div>
-
-            <div className={portfolio.content_container}>
+            <div className={portfolio.content_container} id="portfolio">
                 <h1>Portfolio</h1>
                 <div className={portfolio.tech_icons}>
                     <div>
@@ -101,7 +100,7 @@ export default function Home() {
                 </div>
                 <div className={portfolio.projects}>
                     <div>
-                        <div>
+                        {/* <div> */}
                             <Player src="/videos/Netflix_Video.mp4" muted autoPlay loop fluid>
                                 <ControlBar disableCompletely />
                             </Player>
@@ -109,28 +108,39 @@ export default function Home() {
                                 <h2>Netflix - Landing Page</h2>
                                 <p>Clone da página de apresentação da Netflix feito com React JS</p>
                                 <div className={portfolio.button_wrapper}>
-                                    <a href="#">Live Demo</a>
-                                    <a href="#">Github Repo</a>
+                                    <a href="https://nflix-clone.herokuapp.com" target="_blank">Live Demo</a>
+                                    <a href="https://github.com/HCFerraz/netflix-clone" target="_blank">Github Repo</a>
                                 </div>
                             </div>
-                        </div>
+                        {/* </div> */}
                     </div>
                     <div>
-                        <div>
+                        {/* <div> */}
                             <Player src="/videos/Pokedex_Video.mp4" muted autoPlay loop fluid>
                                 <ControlBar disableCompletely />
                             </Player>
                             <div className={portfolio.description_wrapper}>
                                 <h2>PokeSearch - Consulte seus pokes favoritos</h2>
-                                <p>Clone em React JS de um site de busca de Pokémon feito em HTML, CSS e Javascript durante o curso de Desenvolvimento Front End na Arbyte.</p>
+                                <p>Clone em React JS de um site de busca de Pokémon consumindo a PokeAPI</p>
                                 <div className={portfolio.button_wrapper}>
-                                    <a href="#">Live Demo</a>
-                                    <a href="#">Github Repo</a>
+                                    <a href="https://hcferraz.github.io/pokedex/" target="_blank">Live Demo</a>
+                                    <a href="https://github.com/HCFerraz/pokedex-clone-react" target="_blank">Github Repo</a>
                                 </div>
                             </div>
-                        </div>
+                        {/* </div> */}
                     </div>
                 </div>
+            </div>
+            <div className={contact.content_container} id="contact">
+                <div>
+                    <h1>Entre em contato.</h1>
+                    <div>
+                        <a href="https://www.linkedin.com/in/henrique-cabral-ferraz-8b2b68163/" target="_blank"><img src="/images/linkedin.svg" /></a>
+                        <a href="https://api.whatsapp.com/send/?phone=5511932151911&text&app_absent=0" target="_blank"><img src="/images/whatsapp.svg" /></a>
+                        <a href="mailto:henriquecabralferraz@gmail.com"> <img src="/images/email.png" /> </a>
+                    </div>
+                </div>
+                <img src="/images/contact.svg" />
             </div>
         </div>
     )
